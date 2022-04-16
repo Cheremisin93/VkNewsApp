8e458e8da8ef2d8483e8bbe058f4828acd1281f4
Cheremisin93 <cheremisin9393@mail.ru>
2022-04-16 20:21:52 +0300

    setup vksdk

---
+++ VkNewsApps/Auth/AuthViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  VkNewsApps
+//
+//  Created by Cheremisin Andrey on 16.04.2022.
+//
+
+import UIKit
+import VKSdkFramework
+
+class AuthViewController: UIViewController {
+
+    private var authService: AuthSevice!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        authService = SceneDelegate.shared().authService
+        view.backgroundColor = .red
+        
+        
+    }
+
+    @IBAction func signInTouch(_ sender: UIButton) {
+        authService.wakeUpSession()
+    }
+    
+}
+
---
+++ VkNewsApps/FeedViewController/FeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.swift
+//  VkNewsApps
+//
+//  Created by Cheremisin Andrey on 16.04.2022.
+//
+
+import Foundation
+import UIKit
+
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .cyan
+    }
+}
